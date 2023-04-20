a4855a1ff43e4c0602009fe729309d5140e1b224
Barsha <barshachand4@gmail.com>
2023-04-20 09:41:26 +0545

    This is my first flutter project

---
+++ variables/variables.dart
@@ -0,0 +1,14 @@
+void main(){
+ 
+// variables
+
+  String name = "Barsha";
+  String address = "Kanchanpur";
+  num age=20;
+  bool isMarried = false;
+
+  print("My name is $name");
+   print("My name is $address");
+   print(num);
+   print(isMarried);
+}
