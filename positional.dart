2e0b0e66bd1f6f7b6472a4bfb58de3370b5da9eb
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-04-21 09:58:17 +0545

    Add files via upload

---
+++ positional.dart
@@ -0,0 +1,15 @@
+void printInfo(String name, String college){
+  
+  print("My name is $name, I study in $college");
+}
+
+void main(){
+  // passing values in wrong order
+
+  printInfo("pk","Barsha");
+
+  // passing values in right order
+
+  printInfo("Barsha", "Padma Kanya Multiple Campus");
+
+}
