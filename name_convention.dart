54cd5b6a2c372498352770db7cd49cadcbaa8b30
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-04-30 07:25:56 +0545

    name_convention

---
+++ name_convention.dart
@@ -0,0 +1,11 @@
+void main(){
+// Not standard way
+var fullname = "Barsha Chand";
+// Standard way
+var fullName = "BARSHA CHAND";
+const pi = 3.14;
+
+print("$fullname");
+print("$fullName");
+print(pi);
+}
