4af52db716a77aeca633d90b56728cf07af35e3f
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-05-02 09:36:29 +0545

    variable_type

---
+++ variable_type.dart
@@ -0,0 +1,14 @@
+void main(){
+ 
+// variables
+
+  String name = "Barsha"; // For storing text value. E.g. “John” [Must be in quotes]
+  String address = "Kanchanpur";
+  num age = 20; // For storing any type of number. E.g. 10, 20.2, -20 [both int and double]
+  bool isMarried = false;
+// For storing true or false. E.g. true, false [Only stores true or false values]
+  print("My name is $name");
+   print("My name is $address");
+   print(age);
+   print(isMarried);
+}
