a3aa21026512b17b0bb05e86e4bd0754bcbe2771
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-05-05 06:36:56 +0545

    concatination

---
+++ concatination.dart
@@ -0,0 +1,7 @@
+void main() {   
+String firstName = "BARSHA";
+String lastName = "CHAND";
+print("Full Name is "+firstName + " " + lastName+"."); //Using +
+print("Full name is $firstName $lastName.");  //Using interpolation
+  
+}
