11a0690fd76615e2fda049b7eef565da83d76d53
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-05-06 06:35:26 +0545

    method

---
+++ method.dart
@@ -0,0 +1,9 @@
+//Example of toUpperCase() and toLowerCase()
+void main() { 
+   String fname = "BArsha"; // Here B is capital.
+   String lname = "ChAnd"; // Here C and A are capital.
+   print("First name in uppercase: ${fname.toUpperCase()}"); 
+   print("First name in lowercase: ${fname.toLowerCase()}"); 
+   print("Last name  in uppercase: ${lname.toUpperCase()}"); 
+   print("Last name  in lowercase: ${lname.toLowerCase()}"); 
+}
