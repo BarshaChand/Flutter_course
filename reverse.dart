2475eff3325c8d4e58981b9bebe00ed859796e18
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-05-10 08:54:44 +0545

    reverse

---
+++ reverse.dart
@@ -0,0 +1,6 @@
+// If you want to reverse a String in Dart, you can reverse it using a different solution.
+
+void main() { 
+  String input = "HELLO_WORLD"; 
+  print("$input Reverse is ${input.split('').reversed.join()}"); 
+} 
