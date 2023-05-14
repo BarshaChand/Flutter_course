f261b826c89ac7d22ee3a5bb6f3dd0edd65f3236
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-05-14 20:27:49 +0545

    Add files via upload

---
+++ to_string.dart
@@ -0,0 +1,11 @@
+//Example of toString()
+void main() { 
+int number = 20;     
+String result = number.toString(); 
+  
+  //  The runtimeType property is used to find out the runtime type of the object. 
+  
+print("Type of number is ${number.runtimeType}");  
+print("Type of result is ${result.runtimeType}");  
+    
+}   
