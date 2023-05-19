6b1c6b49586f854d0140217d082fec83a662c54d
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-05-20 00:06:41 +0545

    Add files via upload

---
+++ logical.dart
@@ -0,0 +1,12 @@
+void main(){
+  int a = 123;
+  int b = 456;
+
+    // Printing Info
+    print((a == 123) && (b== 456)); // print true
+    print((a == 1213) && (b== 456)); // print false.
+    print((a == 123) || (b== 456)); // print true.
+    print((a == 1213) || (b== 456)); // print true
+    print((a == 123) != (b== 456));//print false
+
+}
