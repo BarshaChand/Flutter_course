cbd70c761b89a6dfbd61f8069ae60b48caefc4e3
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-05-24 20:46:01 +0545

    Add files via upload

---
+++ multi_line.dart
@@ -0,0 +1,21 @@
+ void main() {
+  
+// Multi Line Using Single Quotes   
+String multiLineText = '''
+Hello my name is Barsha Chand.
+Curently studying in Padma kanya
+I am from Mahendranagr , Kanchanpur
+
+''';
+   
+// Multi Line Using Double Quotes   
+String otherMultiLineText = """
+Hello world
+Hope you all are doing good
+Best Wishes!!
+""";
+   
+// Printing Information   
+print("Multiline text is $multiLineText");
+print("Other multiline text is $otherMultiLineText");
+}
