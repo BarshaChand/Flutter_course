22bc892117c134ae0e714e15076dad41ab9daf81
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-06-08 07:30:39 +0545

    Add files via upload

---
+++ Q_N2.dart
@@ -0,0 +1,9 @@
+// Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+
+void main(){
+  String name1 = 'John Doe';
+  String name2 = "John Doe";
+
+  print("Single quoted name is writen as $name1");
+  print("Double quoted name is writen as $name2");
+}
