1b77b644f370c9aa503ab681de36d642816ace4b
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-06-10 21:15:35 +0545

    Add files via upload

---
+++ Q_N4.dart
@@ -0,0 +1,10 @@
+// Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+
+void main() {
+double p = 5000; // IN Rs
+double t = 5;  // IN Years
+double r = 2; // rate of interest
+double formula= (p * t * r) / 100;
+
+print("The simple interest of $p Rs, $t years, and $r % is : $formula");
+}
