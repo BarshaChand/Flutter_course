bd35c86097c435c1e95c18013d9aebbed89ea442
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-06-24 06:23:10 +0545

    Add files via upload

---
+++ loop.dart
@@ -0,0 +1,9 @@
+/*
+Loops In List
+You can use for loop, for each loop, or any other type of loop.
+*/
+
+void main() {
+  List<int> list = [10, 20, 30, 40, 50];
+  list.forEach((n) => print(n));
+}
