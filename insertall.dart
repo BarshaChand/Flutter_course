38067e32387c77ae775a7960e5b3bf37706957b1
Barsha Chand <109261175+BarshaChand@users.noreply.github.com>
2023-09-10 12:04:28 +0545

    Add files via upload

---
+++ insertall.dart
@@ -0,0 +1,11 @@
+/*
+Example 4: Insert Items To List
+In this example below, we are adding items to myList using insertAll() method.
+*/
+void main() {
+  var myList = [3, 4, 2, 5];
+  print(myList);
+  myList.insertAll(1, [6, 7, 10, 9]);
+  print(myList);
+}
+  
